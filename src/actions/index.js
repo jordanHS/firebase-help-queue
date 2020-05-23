57962b0a7471ffd7d0ee84c3ac64600bf3240f2d
Jordan Safford <jordansafford@gmail.com>
2020-05-23 15:12:50 -0700

    fix errors

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,6 @@
 import * as c from './ActionTypes';
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
-import { createFirestoreInstance }from 'redux-firestore';
-import firebase from "./firebase";
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer);
 
-const rrfProps = {
-  firebase,
-  config: {
-    userProfile: "users"
-  },
-  dispatch: store.dispatch,
-  createFirestoreInstance
-}
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
-    </ReactReduxFirebaseProvider>
-  </Provider>,
-  document.getElementById('root')
-)
 
 export const deleteTicket = id => ({
   type: c.DELETE_TICKET,
